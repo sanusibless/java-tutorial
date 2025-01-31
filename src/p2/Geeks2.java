c54477840d955f86c20631fe9a331c66f001f51d
Blessing Sanusi <holuwoleh@gmail.com>
2025-01-31 09:32:59 +0100

    demostrating implementation of abstract class

--- src/p2/Geeks2.java
+++ src/p2/Geeks2.java
@@ -1,6 +1,18 @@
 package p2;
 
 
-public class Geeks2 {
-    
+abstract class GeeksHelp {
+    abstract void getJavaVersion();
+}
+
+public class Geeks2 extends GeeksHelp {
+    public static void main(String[] args) {
+        Geeks2 geek = new Geeks2();
+
+        geek.getJavaVersion();
+    }
+
+    @Override void getJavaVersion() {
+        System.out.println(Runtime.version());
+    }
 }
